0582f0180649bd62b89810558bccfdaa33670b4e
Tauqir <tauqirkhann.11@gmail.com>
2025-01-02 19:55:17 +0530

    feat: add environment variables and use it

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
   host: "localhost", // or wherever the db is hosted
-  user: "<role_name>",
+  user: process.env.role_name,
   database: "top_users",
-  password: "<role_password>",
+  password: process.env.role_password,
   port: 5432, // The default port
 });
